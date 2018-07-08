5a04e07066d75d056e4b69bec1de4461b587e05e
Michele Tessaro <michele.tessaro@email.it>
2018-07-08 16:17:24 +0200

    Added configuration for deployment to pypi and install with pip

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+    name="plantuml-markdown",
+    version="1.2.3",
+    author="Michele Tessaro",
+    author_email="michele.tessaro@email.it",
+    description="A PlantUML plugin for Markdown",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mikitex70/plantuml-markdown",
+    packages=setuptools.find_packages(),
+    install_requires=['Markdown'],
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 6 - Mature",
+        "Environment :: Plugins",
+        "Topic :: Text Processing"
+    ],
+)
