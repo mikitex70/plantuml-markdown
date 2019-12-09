8918412e8505fb30d6986f6ab948954d64b7262c
Michele Tessaro <michele.tessaro@email.it>
2019-12-09 20:52:25 +0100

    new: usr: added support for Python 3.8

--- test/test_plantuml.py
+++ test/test_plantuml.py
@@ -26,12 +26,27 @@ class PlantumlTest(TestCase):
             return f.read()[:-1]  # skip the last newline
 
     FAKE_IMAGE = 'ABCDEF=='
+    IMAGE_REGEX = re.compile(r'<(?:img|.*object)(?:( alt=".*?")|( class=".*?")|( title=".*?")|( style=".*?")|( src=".*?")|(?:.*?))+/>')
     BASE64_REGEX = re.compile(
         r'("data:image/[a-z+]+;base64,)(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?')
 
     @classmethod
     def _stripImageData(cls, html):
-        return cls.BASE64_REGEX.sub(r'\1%s' % cls.FAKE_IMAGE, html)
+        def sort_attributes(groups):
+            alt = next(x for x in groups if x.startswith(' alt='))
+            title = next(x for x in groups if x.startswith(' title='))
+            classes = next(x for x in groups if x.startswith(' class='))
+            style = next(iter(x for x in groups if x and x.startswith(' style=')), None)
+            src = next(iter(x for x in groups if x and x.startswith(' src=')), None)
+
+            style = style if style and '""' not in style else ''
+            src = src if src and '""' not in src else ''
+
+            html = "<img{}{}{}{}{}/>".format(alt, title, classes, style, src)
+            return cls.BASE64_REGEX.sub(r'\1%s' % cls.FAKE_IMAGE, html)
+
+        return cls.IMAGE_REGEX.sub(lambda x: sort_attributes(x.groups()), html)
+        #return cls.BASE64_REGEX.sub(r'\1%s' % cls.FAKE_IMAGE, html)
 
     FAKE_SVG = '...svg-body...'
     SVG_REGEX = re.compile(r'<(?:\w+:)?svg(?:( alt=".*?")|( class=".*?")|( title=".*?")|( style=".*?")|(?:.*?))+>.*</(?:\w+:)?svg>')
@@ -113,7 +128,7 @@ class PlantumlTest(TestCase):
         """
         text = self.text_builder.diagram("A --> B").title("Diagram test").build()
         self.assertEqual(
-            '<p><img alt="uml diagram" class="uml" src="data:image/png;base64,%s" title="Diagram test" /></p>' % self.FAKE_IMAGE,
+            self._stripImageData('<p><img alt="uml diagram" class="uml" src="data:image/png;base64,%s" title="Diagram test" /></p>' % self.FAKE_IMAGE),
             self._stripImageData(self.md.convert(text)))
 
     def test_arg_title_characters(self):
@@ -122,8 +137,8 @@ class PlantumlTest(TestCase):
         """
         text = self.text_builder.diagram("A --> B").title("Diagram-test/%&\"").build()
         self.assertEqual(
-            '<p><img alt="uml diagram" class="uml" src="data:image/png;base64,%s" title="Diagram-test/%%&amp;&quot;" /></p>' %
-            self.FAKE_IMAGE, self._stripImageData(self.md.convert(text)))
+            self._stripImageData('<p><img alt="uml diagram" class="uml" src="data:image/png;base64,%s" title="Diagram-test/%%&amp;&quot;" /></p>' % self.FAKE_IMAGE),
+            self._stripImageData(self.md.convert(text)))
 
     def test_arg_title_inline_svg(self):
         """
@@ -131,7 +146,7 @@ class PlantumlTest(TestCase):
         """
         text = self.text_builder.diagram("A --> B").format("svg_inline").title("Diagram test").build()
         self.assertEqual(
-            '<p><svg alt="uml diagram" title="Diagram test" class="uml">%s</svg></p>' % self.FAKE_SVG,
+            self._stripImageData('<p><svg alt="uml diagram" title="Diagram test" class="uml">%s</svg></p>' % self.FAKE_SVG),
             self._stripSvgData(self.md.convert(text)))
 
     def test_arg_alt(self):
@@ -140,7 +155,7 @@ class PlantumlTest(TestCase):
         """
         text = self.text_builder.diagram("A --> B").alt("Diagram test").build()
         self.assertEqual(
-            '<p><img alt="Diagram test" class="uml" src="data:image/png;base64,%s" title="" /></p>' % self.FAKE_IMAGE,
+            self._stripImageData('<p><img alt="Diagram test" class="uml" src="data:image/png;base64,%s" title="" /></p>' % self.FAKE_IMAGE),
             self._stripImageData(self.md.convert(text)))
 
     def test_arg_alt_characters(self):
@@ -149,8 +164,8 @@ class PlantumlTest(TestCase):
         """
         text = self.text_builder.diagram("A --> B").alt("Diagram-test/%&\"").build()
         self.assertEqual(
-            '<p><img alt="Diagram-test/%%&amp;&quot;" class="uml" src="data:image/png;base64,%s" title="" /></p>' %
-            self.FAKE_IMAGE, self._stripImageData(self.md.convert(text)))
+            self._stripImageData('<p><img alt="Diagram-test/%%&amp;&quot;" class="uml" src="data:image/png;base64,%s" title="" /></p>' % self.FAKE_IMAGE),
+            self._stripImageData(self.md.convert(text)))
 
     def test_arg_alt_inline_svg(self):
         """
@@ -158,7 +173,7 @@ class PlantumlTest(TestCase):
         """
         text = self.text_builder.diagram("A --> B").format("svg_inline").alt("Diagram test").build()
         self.assertEqual(
-            '<p><svg alt="Diagram test" title="" class="uml">%s</svg></p>' % self.FAKE_SVG,
+            self._stripImageData('<p><svg alt="Diagram test" title="" class="uml">%s</svg></p>' % self.FAKE_SVG),
             self._stripSvgData(self.md.convert(text)))
 
     def test_arg_classes(self):
@@ -167,7 +182,7 @@ class PlantumlTest(TestCase):
         """
         text = self.text_builder.diagram("A --> B").classes("class1 class2").build()
         self.assertEqual(
-            '<p><img alt="uml diagram" class="class1 class2" src="data:image/png;base64,%s" title="" /></p>' % self.FAKE_IMAGE,
+            self._stripImageData('<p><img alt="uml diagram" class="class1 class2" src="data:image/png;base64,%s" title="" /></p>' % self.FAKE_IMAGE),
             self._stripImageData(self.md.convert(text)))
 
     def test_arg_classes_inline_svg(self):
@@ -176,7 +191,7 @@ class PlantumlTest(TestCase):
         """
         text = self.text_builder.diagram("A --> B").format("svg_inline").classes("class1 class2").build()
         self.assertEqual(
-            '<p><svg alt="uml diagram" title="" class="class1 class2">%s</svg></p>' % self.FAKE_SVG,
+            self._stripImageData('<p><svg alt="uml diagram" title="" class="class1 class2">%s</svg></p>' % self.FAKE_SVG),
             self._stripSvgData(self.md.convert(text)))
 
     def test_arg_format_png(self):
@@ -225,7 +240,7 @@ class PlantumlTest(TestCase):
         """
         text = self.text_builder.diagram("A --> B").width("120px").build()
         self.assertEqual(
-            '<p><img alt="uml diagram" class="uml" src="data:image/png;base64,%s" style="max-width:120px" title="" width="100%%" /></p>' % self.FAKE_IMAGE,
+            self._stripImageData('<p><img alt="uml diagram" class="uml" src="data:image/png;base64,%s" style="max-width:120px" title="" width="100%%" /></p>' % self.FAKE_IMAGE),
             self._stripImageData(self.md.convert(text)))
 
     def test_arg_with_percent(self):
@@ -234,7 +249,7 @@ class PlantumlTest(TestCase):
         """
         text = self.text_builder.diagram("A --> B").width("70%").build()
         self.assertEqual(
-            '<p><img alt="uml diagram" class="uml" src="data:image/png;base64,%s" style="max-width:70%%" title="" width="100%%" /></p>' % self.FAKE_IMAGE,
+            self._stripImageData('<p><img alt="uml diagram" class="uml" src="data:image/png;base64,%s" style="max-width:70%%" title="" width="100%%" /></p>' % self.FAKE_IMAGE),
             self._stripImageData(self.md.convert(text)))
 
     def test_arg_height(self):
@@ -243,7 +258,7 @@ class PlantumlTest(TestCase):
         """
         text = self.text_builder.diagram("A --> B").height("120px").build()
         self.assertEqual(
-            '<p><img alt="uml diagram" class="uml" src="data:image/png;base64,%s" style="max-height:120px" title="" width="100%%" /></p>' % self.FAKE_IMAGE,
+            self._stripImageData('<p><img alt="uml diagram" class="uml" src="data:image/png;base64,%s" style="max-height:120px" title="" width="100%%" /></p>' % self.FAKE_IMAGE),
             self._stripImageData(self.md.convert(text)))
 
     def test_arg_height_percent(self):
@@ -252,7 +267,7 @@ class PlantumlTest(TestCase):
         """
         text = self.text_builder.diagram("A --> B").height("50%").build()
         self.assertEqual(
-            '<p><img alt="uml diagram" class="uml" src="data:image/png;base64,%s" style="max-height:50%%" title="" width="100%%" /></p>' % self.FAKE_IMAGE,
+            self._stripImageData('<p><img alt="uml diagram" class="uml" src="data:image/png;base64,%s" style="max-height:50%%" title="" width="100%%" /></p>' % self.FAKE_IMAGE),
             self._stripImageData(self.md.convert(text)))
 
     def test_arg_width_and_height(self):
@@ -261,7 +276,7 @@ class PlantumlTest(TestCase):
         """
         text = self.text_builder.diagram("A --> B").width("120px").height("120px").build()
         self.assertEqual(
-            '<p><img alt="uml diagram" class="uml" src="data:image/png;base64,%s" style="max-width:120px;max-height:120px" title="" width="100%%" /></p>' % self.FAKE_IMAGE,
+            self._stripImageData('<p><img alt="uml diagram" class="uml" src="data:image/png;base64,%s" style="max-width:120px;max-height:120px" title="" width="100%%" /></p>' % self.FAKE_IMAGE),
             self._stripImageData(self.md.convert(text)))
 
     def test_arg_format_width_svg_inline(self):
@@ -321,12 +336,12 @@ class PlantumlTest(TestCase):
             .indent(4) \
             .diagram('A --> B') \
             .build()
-        self.assertEqual('''<ul>
+        self.assertEqual(self._stripImageData('''<ul>
 <li>
 <p>list item</p>
 <p><img alt="uml diagram" class="uml" src="data:image/png;base64,%s" title="" /></p>
 </li>
-</ul>''' % self.FAKE_IMAGE,
+</ul>''' % self.FAKE_IMAGE),
                           self._stripImageData(self.md.convert(text)))
 
     def test_unicode_chars(self):
