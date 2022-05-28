3a6c6d0b4b20943f5bba95fda4cecc68ac05386a
Michele Tessaro <michele.tessaro@email.it>
2022-05-28 16:02:26 +0200

    @minor removed package build for old pythons

--- release.sh
+++ release.sh
@@ -3,17 +3,8 @@
 [[ -d "build" ]] && rm -rf build
 [[ -d "dist"  ]] && rm -rf dist
 
-# Build package for Python 2
-#[[ -d ".env27" ]] || python2.7 -m virtualenv .env27
-#. .env27/bin/activate
-#pip install setuptools twine wheel
-#python setup.py bdist_wheel
-
-#deactivate
-
 # Build package for Python 3
-[[ -d ".env36" ]] || python3.6 -m virtualenv .env36
-. .env36/bin/activate
+. .env3*/bin/activate
 pip install setuptools twine wheel
 python setup.py bdist_wheel
 
